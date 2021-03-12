19cf3e1a6bf86c62a360aad6871dc63a03dbce50
achrafOuk <achraf.crispy@gmail.com>
2021-03-12 18:56:10 +0100

    polish blog snippets

--- static/javascript/shortText.js
+++ static/javascript/shortText.js
@@ -1,10 +1,15 @@
 text = document.getElementsByClassName("card-text");
 for (let i = 0; i < text.length; i++) {
-  let inputt = text[i].innerText;
-  console.log(":" + inputt);
-  if (inputt !== "undefined" || inputt.length > 0) {
-    if (inputt.length > 100) {
-      text[i].innerText = inputt.slice(0, 100) + "...";
+  let poster = text[i].querySelector("img");
+  let post = text[i].innerText;
+  text[i].innerHTML = "";
+  if (poster !== "undefined") {
+    text[i].append(poster);
+  }
+  if (post !== "undefined" || post.length > 0) {
+    if (post.length > 100) {
+      post = post.slice(0, 100) + "...";
     }
+    text[i].append(post);
   }
 }
