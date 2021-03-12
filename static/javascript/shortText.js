d16bb9364cb080cb445df712fbf8533179acdc0b
achrafOuk <achraf.crispy@gmail.com>
2021-03-12 19:21:46 +0100

    polish blog snippets

--- static/javascript/shortText.js
+++ static/javascript/shortText.js
@@ -3,9 +3,12 @@ for (let i = 0; i < text.length; i++) {
   let poster = text[i].querySelector("img");
   let post = text[i].innerText;
   text[i].innerHTML = "";
-  if (poster !== "undefined") {
-    text[i].append(poster);
+  if (!poster) {
+    poster = document.createElement("img");
+    poster.src =
+      "https://www.ecpgr.cgiar.org/fileadmin/templates/ecpgr.org/Assets/images/No_Image_Available.jpg";
   }
+  text[i].append(poster);
   if (post !== "undefined" || post.length > 0) {
     if (post.length > 100) {
       post = post.slice(0, 100) + "...";
