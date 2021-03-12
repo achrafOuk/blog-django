2c2e34733e58e61916b7c78cf071bc6288c6d03e
achrafOuk <achraf.crispy@gmail.com>
2021-03-12 17:29:50 +0100

    add text editor

--- blog_app/models.py
+++ blog_app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 from PIL import Image
+from ckeditor.fields import RichTextField
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -33,7 +34,7 @@ class Posts(models.Model):
     post_id = models.AutoField(primary_key=True)
     title=models.CharField(max_length=220,blank=False, null=False)
     #image=models.ImageField(default='http://0.gravatar.com/avatar/3f009d72559f51e7e454b16e5d0687a1?s=96&d=mm&r=g', upload_to='profile_pics')
-    content=models.TextField(blank=True, null=True)
+    content=RichTextField(blank=True, null=True)
     date_posted=models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     views =models.IntegerField(default=0)
